2f0fd7a5651bb21b8d83381125ccc3143d272f9e
hyuk12 <hotman7025@naver.com>
2022-12-28 17:00:19 +0900

    day01 출력

---
+++ workspace/JavaStudy_aws_202212_haehyuk/src/j01_출력/HelloJava.java
@@ -0,0 +1,21 @@
+package j01_출력;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		// tab 들여쓰기 코드의 가독성을 높인다.
+        // 주석: 코드 내부에 메모를 할 때 사용을 한다.
+        // 컴파일시에 무시된다.
+        // 한줄 주석
+        /*
+        	여러줄 주석
+        	1
+        	2
+        	3
+        */
+		
+		/**
+		 *  클래스, 메소드 등의 정보를 설명하기 위한 주석
+		 */
+		System.out.println("Hello World!");
+	}
+}
