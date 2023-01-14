32f3488581adae9a933bab3e0c03c00574a1cef9
hyuk12 <hotman7025@naver.com>
2023-01-14 19:42:25 +0900

    추상/ 인터페이스/ 상속 예제 완료

--- workspace/JavaStudy_aws_202212_haehyuk/src/j_exam/FurnitureRepository.java
+++ workspace/JavaStudy_aws_202212_haehyuk/src/j_exam/FurnitureRepository.java
@@ -1,7 +1,63 @@
 package j_exam;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FurnitureRepository {
-    public void save () {
+    private Furniture furniture;
+    private List<Furniture> furnitures;
+
+    private static final int CODE = 20230000;
+    private static int ai = 1;
+
+    private int furnitureCode;
+    private String productName;
+    private String type;
+    private String colors;
+
+    public FurnitureRepository () {
+        furnitures = new ArrayList<>();
 
     }
+
+    public void addFurniture(Furniture furniture){
+        furniture.setFurnitureCode(CODE + ai);
+        ai++;
+
+        for (int i = 0; i < furnitures.size(); i++) {
+            if(furnitures.get(i) == null) {
+                furnitures.add(furniture);
+                return;
+            }
+        }
+        System.out.println();
+    }
+
+    public void insertFurniture(Furniture furniture) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("구매하실 제품의 정보를 입력해주세요");
+
+        System.out.print("가구의 종류: ");
+        String type = scanner.nextLine();
+
+        System.out.print("가구 색깔: ");
+        String color = scanner.nextLine();
+
+        System.out.print("제품이름: ");
+        String productName = scanner.nextLine();
+
+        System.out.println("============================");
+
+
+        furniture.setFurnitureCode(CODE + ai);
+        furniture.setType(type);
+        furniture.setColor(color);
+        furniture.setProductName(productName);
+
+        addFurniture(furniture);
+    }
+
+
 }
--- workspace/JavaStudy_aws_202212_haehyuk/src/j_exam/Main.java
+++ workspace/JavaStudy_aws_202212_haehyuk/src/j_exam/Main.java
@@ -3,10 +3,14 @@ package j_exam;
 public class Main {
 
 	public static void main(String[] args) {
-		Bed b1 = new Bed();
-		b1.productName = "침대";
-		FurnitureFactory factory = new FurnitureFactory(b1);
-		System.out.println(factory.make());
+		Furniture f1 = new Bed();
+		Furniture f2 = new Bed();
 
+
+
+		FurnitureFactory.getInstance().make(f1);
+		FurnitureFactory.getInstance().make(f2);
+		FurnitureFactory.getInstance().pave(f1);
+		FurnitureFactory.getInstance().deliver(f1);
 	}
 }
