a81d489a901ee10438165b272fb749c9c14add7d
hyuk12 <hotman7025@naver.com>
2023-03-24 09:38:16 +0900

    변경점

--- workspace/react/input_study/src/components/Input/style.js
+++
@@ -1,7 +0,0 @@
-import {css} from "@emotion/react";
-
-export const style = css`
-      border: none;
-      border-bottom: 1px solid #dbdbdb;
-      outline: none;
-    `;
--- workspace/react/input_study/src/components/Td/style.js
+++
@@ -1,6 +0,0 @@
-import {css} from "@emotion/react";
-
-export const style = css`
-      border: 1px solid #dbdbdb;
-      padding: 15px;
-    `;
